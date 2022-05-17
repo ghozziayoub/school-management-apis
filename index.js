74be2f978c1b3c5b8dd63d44ed86afdc1284c643
Ayoub Ghozzi <ghozziayoub@gmail.com>
2022-05-17 12:27:52 +0100

    add api notes file

--- index.js
+++ index.js
@@ -9,40 +9,7 @@ app.use(express.json())
 
 const port = 3000
 
-app.get("/a3tini-donnee/:userId", (req, res) => {
 
-    let myId = req.params.userId
-
-    if (isNaN(myId)) {
-        res.status(400).send({ message: "Invalid ID !" })
-    } else {
-
-        let users = [
-            { id: 1, name: "ali", age: "50" },
-            { id: 2, name: "mohamed", age: "30" },
-            { id: 3, name: "salah", age: "60" },
-        ]
-
-        let user = users.find((u) => { return u.id == myId })
-
-        if (user) {
-            res.status(200).send(user)
-        } else {
-            res.status(404).send({ message: "user not found" })
-        }
-    }
-
-
-})
-
-app.post("/register", (req, res) => {
-
-    let user = req.body
-
-    console.log("index-L42", user);
-
-    res.status(200).send({ message: "register works !" })
-})
 
 // create server
 app.listen(port, () => { console.log(`🟢 Server started on port ${port}`); })
