f6d4ab2f2e50450425f6fa8f37bbc92d3bdb7eab
ahmed <ahmedachour789@gmail.com>
2022-05-19 11:34:58 +0100

    prepare all apis

--- controllers/trainersController.js
+++ controllers/trainersController.js
@@ -2,9 +2,35 @@ const express = require("express")
 
 const app = express()
 
-app.get('/all', (req, res) => {
+let trainer = [];
+
+//add trainer api
+app.post('/', (req, res) => {
     res.status(200).send({ message: "trainer all works !" })
 })
+
+//show all trainer api
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "trainer all works !" })
+})
+
+//show spesific trainer with id api
+app.get('/:id', (req, res) => {
+    res.status(200).send({ message: "trainer all works !" })
+})
+
+
+//modify trainer with spesific id
+app.patch('/:id', (req, res) => {
+    res.status(200).send({ message: "trainer all works !" })
+})
+
+
+//delete trainer
+app.get('/:id', (req, res) => {
+    res.status(200).send({ message: "trainer all works !" })
+})
+
 
 
 module.exports = app
