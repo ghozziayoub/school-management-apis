1101cec92b340fd09b71b409949a185e52af78d6
Rania Ferjeni <rania0ferjani@gmail.com>
2022-05-19 11:52:00 +0000

    init get all Api

--- controllers/trainingsController.js
+++ controllers/trainingsController.js
@@ -6,5 +6,18 @@ app.get('/', (req, res) => {
     res.status(200).send({ message: "Post Training !" })
 })
 
+app.get('/all', (req, res) => {
+    res.status(200).send({ message: "Get all Training works !" })
+})
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "training all works !" })
+})
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "training all works !" })
+})
+
+app.get('/all', (req, res) => {
+    res.status(200).send({ message: "Get all Training works !" })
+})
 
 module.exports = app
