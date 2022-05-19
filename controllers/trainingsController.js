9ac6f8b8e88e3af6d6fdda5f6879d2247fb5e317
Rania Ferjeni <rania0ferjani@gmail.com>
2022-05-19 11:43:11 +0000

    lint post Api

--- controllers/trainingsController.js
+++ controllers/trainingsController.js
@@ -2,8 +2,8 @@ const express = require("express")
 
 const app = express()
 
-app.get('/all', (req, res) => {
-    res.status(200).send({ message: "training all works !" })
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "Post Training !" })
 })
 
 
