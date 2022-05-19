f5ccc63669224637baa2936a3ba13c5c8182277b
Likrania <kraiemkhalil35@gmail.com>
2022-05-19 13:17:30 +0100

    added messages-filles

---
+++ models/messages.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const messagesSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+const message = mongoose.model("message", messagesSchema)
+
+module.exports = message
