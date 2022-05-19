65ea046818ef6e99763b7443056fc7a346a37461
Rania Ferjeni <rania0ferjani@gmail.com>
2022-05-19 12:16:44 +0000

    init add api

--- controllers/trainingsController.js
+++ controllers/trainingsController.js
@@ -2,33 +2,10 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.Post('/', (req, res) => {
     res.status(200).send({ message: "Post Training !" })
 })
 
-app.get('/all', (req, res) => {
-    res.status(200).send({ message: "Get all Training works !" })
-})
-app.get('/', (req, res) => {
-    res.status(200).send({ message: "training all works !" })
-})
-app.get('/', (req, res) => {
-    res.status(200).send({ message: "training all works !" })
-})
-
-app.get('/all', (req, res) => {
-    res.status(200).send({ message: "Get all Training works !" })
-})
-app.get('/:id', (req, res) => {
-    res.status(200).send({ message: "Get by id Training works !" })
-})
-
-app.get('/:update', (req, res) => {
-    res.status(200).send({ message: " patch by id Training works !" })
-})
-app.get('/:Delete', (req, res) => {
-    res.status(200).send({ message: "delete by id Training works !" })
-})
 
 
 module.exports = app
