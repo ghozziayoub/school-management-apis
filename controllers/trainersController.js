661b628d9c14b23e304bca189a3c573db50cf71e
ahmed <ahmedachour789@gmail.com>
2022-05-24 13:17:41 +0100

    post user api saved bd

--- controllers/trainersController.js
+++ controllers/trainersController.js
@@ -1,10 +1,24 @@
 const express = require("express")
+const Trainer = require("../models/trainer")
 
 const app = express()
 
 //add trainer api
-app.post('/', (req, res) => {
-    res.status(200).send({ message: "add trainer works !" })
+app.post('/', async (req, res) => {
+    try {
+        let data = req.body
+        let trainer = new Trainer({
+            firstname: data.firstname,
+            lastname: data.lastname,
+            email: data.email,
+            speciality: data.speciality,
+            yearsOfExperience: data.yearsOfExperience
+        })
+        await trainer.save()
+        res.status(201).send({message:"trainer added successfully"})
+    } catch (error) {
+        res.status(400).send({message:"trainer not saved !", error: error})
+    }
 })
 
 //show all trainers api
