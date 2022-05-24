e45775f3eaabc4291820c18e9fdce404ae3f4813
Ayoub Ghozzi <ghozziayoub@gmail.com>
2022-05-24 11:15:57 +0100

    connect to mongo database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose")
 
+let DATABASE_URI = "mongodb://localhost:27017"
+let DATABASE_NAME = "school-management"
+
 mongoose
-    .connect("mongodb://localhost:27017")
+    .connect(`${DATABASE_URI}/${DATABASE_NAME}`)
     .then(() => { console.log("Connection to database success ! "); })
     .catch(() => { console.log("Error Connection to database ! "); })
 
