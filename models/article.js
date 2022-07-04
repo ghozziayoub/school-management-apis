b1c4d1d81d219d64e9892d1c4d661d24df9237a2
ahmed <ahmedachour789@gmail.com>
2022-07-04 11:30:05 +0100

    feature:add_article_model

---
+++ models/article.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const ArticleSchema = mongoose.Schema({
+
+    titre:{
+        type: String,
+        required: true,
+        min:3,
+        max:160, 
+    },
+    content:{
+        type: String,
+        required: true,
+        min:200,
+        max:2000000,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    createdAt:{
+        type: Date,
+        required: true
+    },
+    createdBy:{
+        type: String,
+        required: true
+    }
+    
+
+});
+
+const Article = mongoose.model("article", ArticleSchema);
+
+module.exports = Article;
