f967577f45c1890af619715f191b10c220644091
ahmed <ahmedachour789@gmail.com>
2022-07-12 14:10:24 +0100

    patch: update training controller

--- controllers/trainingsController.js
+++ controllers/trainingsController.js
@@ -120,7 +120,7 @@ app.get("/:id", async (req, res) => {
       program: training.program,
       hours: training.hours,
       image: training.image,
-      price:training.price,
+      price: training.price,
       starting_date: training.starting_date,
       trainer,
       category,
@@ -137,25 +137,22 @@ app.get("/:id", async (req, res) => {
 
 app.patch("/:id", [upload.single("picture")], async (req, res) => {
   try {
-    let trainingId = req.params.id;;
-    let data = req.body
+    let trainingId = req.params.id;
+    let data = req.body;
     if (req.file) {
       data.image = req.file.filename;
       let trainingPic = await Training.findOne({ _id: trainingId });
-        fs.unlinkSync("assets/images/trainings/" + trainingPic.image);
+      fs.unlinkSync("assets/images/trainings/" + trainingPic.image);
     }
     let trainer = await Trainer.findOne({ _id: data.idTrainer });
     let category = await Category.findOne({ _id: data.idCategory });
     if (trainer && category) {
       let training = await Training.findOneAndUpdate({ _id: trainingId }, data);
-      if (training) 
-        res.status(200).send({ message: "Training updated !" });
-      else 
-        res.status(404).send({ message: "Training not found !" });
+      if (training) res.status(200).send({ message: "Training updated !" });
+      else res.status(404).send({ message: "Training not found !" });
     } else {
       res.status(400).send({ message: "Training not saved !", error: error });
     }
- 
   } catch (error) {
     res
       .status(400)
@@ -167,7 +164,7 @@ app.delete("/:id", async (req, res) => {
   try {
     let trainingId = req.params.id;
     console.log(trainingId);
-    
+
     let trainingPic = await Training.findOne({ _id: trainingId });
     fs.unlinkSync("assets/images/trainings/" + trainingPic.image);
     let training = await Training.findOneAndDelete({ _id: trainingId });
